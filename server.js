052aaf9ee3fa5408acb0f8dab5600fe9253e5082
Dhruvik <barvaliyadhruvik7@gmail.com>
2021-07-18 17:17:44 +0530

    Compalte Project

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-if (process.env.NODE_ENV !== "production") {
-
-    require('dotenv').load()
-    // require('dotenv').config()
-
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load()
 }
 
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
-const bodyParser = require("body-parser")
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
@@ -18,16 +16,15 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({limit:"10mb",extended:false}))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
-
-const mongoose = require("mongoose")
-const { urlencoded } = require('body-parser')
+const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
-db.on("error", error => console.log(error))
-db.once("open", () => console.log("Database Connection Successfully Done"))
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
